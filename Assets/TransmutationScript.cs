ad62b8db856222ac573ccb8606b4e5f187955a1a
simongreene02 <simongreene02@gmail.com>
2017-10-01 13:48:59 -0500

    Initial commit

---
+++ Assets/TransmutationScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransmutationScript : MonoBehaviour {
+
+	public GameObject cube;
+	private bool hasBeenClicked = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.GetComponent<MeshRenderer> ().enabled = !hasBeenClicked;
+		cube.gameObject.GetComponent<MeshRenderer> ().enabled = hasBeenClicked;
+	}
+
+	public void OnBeingClicked() {
+		hasBeenClicked = true;
+	}
+}
